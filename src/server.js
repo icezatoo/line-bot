59ceb517ecfb1692199f2d52df9e4845562c0be8
ice <icefrontdev@gmail.com>
2019-03-27 10:52:33 +0700

    Add channel Accesstoken  and Line webhook

--- src/server.js
+++ src/server.js
@@ -1,18 +1,40 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import { middleware, Client } from '@line/bot-sdk'
 
+dotenv.config()
 export const app = express()
 export const port = process.env.PORT || 4000
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+}
+
+const client = new Client(config)
 
 app.disable('x-powered-by')
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.sendStatus(200)
+app.post('/webhook', middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then(result => res.json(result))
 })
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null)
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  })
+}
 
 export const start = () => {
   app.listen(port, () => {
